3040042f652c2cae81b02d4e4f12a64b33da6aba
thebinary <binary4bytes@gmail.com>
2017-09-04 10:28:51 +0545

    segreagate tuple rows and dict rows get functions

--- teasysnmp/session.py
+++ teasysnmp/session.py
@@ -3,17 +3,15 @@ from easysnmp import Session
 
 class Session(Session):
 
-    def get_tbl_row(self, table, index, *fields):
-        """Get given fields from table row of given index
+    def __get_tbl_row(self, table, index, *fields):
+        """SNMP get given fields of given index row
 
-        :param table: easysnmp table tuple
+        :param table: teasysnmp table
         :param index: the index that defines the row in the snmp table
-        :returns: dictionary of fields and values
+        :returns: snmp get results
         :rtype:
 
-        NOTE: If no fields are given all fields of the table are fetched
         """
-
         # If no fields passed get all fields in the table
         if fields is ():
             fields = table[1].keys()
@@ -24,13 +22,59 @@ class Session(Session):
             for field in fields
         ]
 
-        snmp_result = self.get(oids)
+        return self.get(oids)
+
+    def get_tbl_row(self, table, index, *fields):
+        """Get given fields from table row of given index
+
+        :param table: teasysnmp table tuple
+        :param index: the index to fetch
+        :returns: tuple of index and given field values of the row
+        :rtype:
+
+        """
+        snmp_result = self.__get_tbl_row(self, table, index, *fields)
+
+        data = [
+            entry.value
+            for entry in snmp_result
+        ]
+        data.insert(0, index)
+
+        return data
+
+    def get_tbl_rows(self, table, indexes, *fields):
+        """Get given fields from tbale rows of given indexes
+
+        :param table: teasysnmp table tuple
+        :param indexes: the indexes to fetch
+        :returns: list of tuples of index and field values
+        :rtype:
+
+        """
+        return [
+            self.get_tbl_row(self, table, index, *fields)
+            for index in indexes
+        ]
+
+    def get_tbl_row_dict(self, table, index, *fields):
+        """Get given fields from table row of given index
+
+        :param table: easysnmp table tuple
+        :param index: the index that defines the row in the snmp table
+        :returns: dictionary of fields and values
+        :rtype:
+
+        NOTE: If no fields are given all fields of the table are fetched
+        """
+        snmp_result = self.__get_tbl_row(self, table, index, *fields)
+
         return {
             fields[i]: entry.value
             for i, entry in enumerate(snmp_result)
         }
 
-    def get_tbl_rows(self, table, indexes, *fields):
+    def get_tbl_rows_dict(self, table, indexes, *fields):
         """Get given fields from tables rows of given indexes
 
         :param table: easysnmp table tuple
@@ -41,7 +85,7 @@ class Session(Session):
         NOTE: If no fields are given all fields of the table are fetched
         """
         return {
-            index: self.get_tbl_row(table, index, *fields)
+            index: self.get_tbl_row_dict(table, index, *fields)
             for index in indexes
         }
 
